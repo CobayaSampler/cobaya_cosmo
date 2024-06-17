221356a550425acb47d035d6f0f1b9b4de03083e
JesusTorrado <JesusTorrado@users.noreply.github.com>
2024-06-17 11:35:50 +0200

    external: initial implementation and some cmb likelihoods

---
+++ cobaya_cosmo/external/external_list.py
@@ -0,0 +1,74 @@
+cmb = {
+    "planck_PR4_lollipop.lowlE": {
+        "class": "planck_2020_lollipop.lowlE",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_lollipop",
+            "min_version": "4.1.1",
+        },
+    },
+    "planck_PR4_lollipop.lowlB": {
+        "class": "planck_2020_lollipop.lowlB",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_lollipop",
+            "min_version": "4.1.1",
+        },
+    },
+    "planck_PR4_lollipop.lowlEB": {
+        "class": "planck_2020_lollipop.lowlEB",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_lollipop",
+            "min_version": "4.1.1",
+        },
+    },
+    "planck_PR4_hillipop.TT": {
+        "class": "planck_2020_hillipop.TT",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_hillipop",
+            "min_version": "4.2.2",
+        },
+    },
+    "planck_PR4_hillipop.TE": {
+        "class": "planck_2020_hillipop.TE",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_hillipop",
+            "min_version": "4.2.2",
+        },
+    },
+    "planck_PR4_hillipop.EE": {
+        "class": "planck_2020_hillipop.EE",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_hillipop",
+            "min_version": "4.2.2",
+        },
+    },
+    "planck_PR4_hillipop.TTTEEE": {
+        "class": "planck_2020_hillipop.TTTEEE",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "pip": "planck_2020_hillipop",
+            "min_version": "4.2.2",
+        },
+    },
+    "planck_PR4_lensing": {
+        "class": "planckpr4lensing",
+        "desc": "sth...",
+        "biburl": "http...",
+        "package_install": {
+            "github_repository": "carronj/planck_PR4_lensing",
+            "min_version": "1.0.2",
+        },
+    },
+}
