a02e5960a9ef3c2fd39b70191d5ada787fe41ffa
JesusTorrado <JesusTorrado@users.noreply.github.com>
2024-06-17 15:24:14 +0200

    external_list: wmap and urls

--- cobaya_cosmo/external/external_list.py
+++ cobaya_cosmo/external/external_list.py
@@ -3,6 +3,7 @@ cmb = {
     "planck_PR4_lollipop.lowlE": {
         "class": "planck_2020_lollipop.lowlE",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/lollipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_lollipop",
@@ -12,6 +13,7 @@ cmb = {
     "planck_PR4_lollipop.lowlB": {
         "class": "planck_2020_lollipop.lowlB",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/lollipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_lollipop",
@@ -21,6 +23,7 @@ cmb = {
     "planck_PR4_lollipop.lowlEB": {
         "class": "planck_2020_lollipop.lowlEB",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/lollipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_lollipop",
@@ -30,6 +33,7 @@ cmb = {
     "planck_PR4_hillipop.TT": {
         "class": "planck_2020_hillipop.TT",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/hillipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_hillipop",
@@ -39,6 +43,7 @@ cmb = {
     "planck_PR4_hillipop.TE": {
         "class": "planck_2020_hillipop.TE",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/hillipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_hillipop",
@@ -48,6 +53,7 @@ cmb = {
     "planck_PR4_hillipop.EE": {
         "class": "planck_2020_hillipop.EE",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/hillipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_hillipop",
@@ -57,6 +63,7 @@ cmb = {
     "planck_PR4_hillipop.TTTEEE": {
         "class": "planck_2020_hillipop.TTTEEE",
         "desc": "sth...",
+        "url": "https://github.com/planck-npipe/hillipop",
         "biburl": "http...",
         "package_install": {
             "pip": "planck_2020_hillipop",
@@ -66,6 +73,7 @@ cmb = {
     "planck_PR4_lensing": {
         "class": "planckpr4lensing",
         "desc": "Planck PR4 (NPIPE) lensing likelihoods, by J. Carron, M. Mirmelstein and A. Lewis, together with the Planck PR4 ISW-lensing likelihoods by J. Carron, G. Fabbian and A. Lewis",
+        "url": "https://github.com/carronj/planck_PR4_lensing",
         "biburl": ["https://arxiv.org/abs/2007.04997", "https://arxiv.org/abs/2005.05290"],
         "package_install": {
             "github_repository": "carronj/planck_PR4_lensing",
@@ -76,6 +84,7 @@ cmb = {
     "act_dr4_lite": {
         "class": "pyactlike.ACTPol_lite_DR4",
         "desc": "Atacama Cosmology Telescope DR4 CMB power spectrum likelihood, already marginalized over SZ and foreground emission; based on the WMAP and ACT team's likelihood software.",
+        "url": "https://github.com/ACTCollaboration/pyactlike",
         "biburl": ["https://arxiv.org/abs/2007.07288", "https://arxiv.org/abs/2007.07289"],
         "package_install": {
             "github_repository": "ACTCollaboration/pyactlike",  # BRANCH cobaya_updates???
@@ -84,6 +93,7 @@ cmb = {
     "act_dr4_lite_for_planck_combination": {
         "class": "pyactlike.ACTPol_lite_DR4_for_combining_with_planck",
         "desc": "Atacama Cosmology Telescope DR4 CMB power spectrum likelihood, already marginalized over SZ and foreground emission, with a multipole cut to avoid correlations with Plank; based on the WMAP and ACT team's likelihood software.",
+        "url": "https://github.com/ACTCollaboration/pyactlike",
         "biburl": ["https://arxiv.org/abs/2007.07288", "https://arxiv.org/abs/2007.07289"],
         "package_install": {
             "github_repository": "ACTCollaboration/pyactlike", # BRANCH cobaya_updates???
@@ -92,6 +102,7 @@ cmb = {
     "act_dr6_lenslike": {
         "class": "act_dr6_lenslike.ACTDR6LensLike",
         "desc": "...",
+        "url": "https://github.com/ACTCollaboration/act_dr6_lenslike",
         "biburl": ["https://arxiv.org/abs/2304.05203", "https://arxiv.org/abs/2304.05202"],
         "package_install": {
             "pip": "act_dr6_lenslike",
@@ -102,6 +113,7 @@ cmb = {
     "sptpol_2017.TEEE": {
         "class": "sptpol_2017.TEEE",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/1707.09353",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -110,6 +122,7 @@ cmb = {
     "spt3g_2020.TEEE": {
         "class": "spt3g_2020.TEEE",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2101.01684",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -118,6 +131,7 @@ cmb = {
     "spt_hiell_2020.TT": {
         "class": "spt_hiell_2020.TT",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2002.06197",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -126,6 +140,7 @@ cmb = {
     "spt3g_2022.TT": {
         "class": "spt3g_2022.TT",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2212.05642",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -134,6 +149,7 @@ cmb = {
     "spt3g_2022.TE": {
         "class": "spt3g_2022.TE",
         "desc": "",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2212.05642",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -142,6 +158,7 @@ cmb = {
     "spt3g_2022.EE": {
         "class": "spt3g_2022.EE",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2212.05642",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
@@ -150,9 +167,21 @@ cmb = {
     "spt3g_2022.TTTEEE": {
         "class": "spt3g_2022.TTTEEE",
         "desc": "...",
+        "url": "https://github.com/xgarrido/spt_likelihoods",
         "biburl": "https://arxiv.org/abs/2212.05642",
         "package_install": {
             "github_repository": "xgarrido/spt_likelihoods",
+        },
+    },
+    # WMAP ###############################################################################
+    "wmap_v5": {
+        "class": "wmaplike.WMAPLike",
+        "desc": "WMAP likelihood implementation by H. Jense of the WMAP v5 likelihood, retaining only data in temperature and high-l polarization.",
+        "url": "https://github.com/HTJense/pyWMAP",
+        "biburl": "https://arxiv.org/pdf/1212.5225",
+        "package_install": {
+            "github_repository": "HTJense/pyWMAP",
+            "min_version": "0.1.1",
         },
     },
 }
